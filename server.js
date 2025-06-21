ea2506b9811af0316344e68913854d344f7fd8b7
unknown <sujaysahis73@gmail.com>
2025-06-21 11:12:44 +0530

    ADD TOKENS ON SERVER

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization", "dToken","aToken",],
 }));
 app.use(express.json());
 
